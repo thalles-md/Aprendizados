87797bf6cafe64ff0629793cf1a2724afe979741
Thalles <thallyslira@gmail.com>
2026-01-01 18:06:09 -0300

    Primeiro Commit & Push do Ano

---
+++ Python/string.py
@@ -0,0 +1,15 @@
+# nome = input(str("Qual o seu nome? "))
+# idade = input(("Qual a sua idade? "))
+
+# Concatenar e Atribuir à uma nova string:
+
+# tamanho = len(nome)
+
+# print(f"Seu nome é {nome}")
+# print(f"Sua idade é {idade}")
+# print(f"O tamanho do seu nome é de {tamanho} letras")
+
+# Retirar pedaços
+
+nome = input(("Qual o seu nome? "))
+print(f"As primeiras letras do seu nome são '{(nome[0:3])}'")
