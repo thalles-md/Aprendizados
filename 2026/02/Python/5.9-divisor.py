c46a9bcf73ada816df7f99846db1341b9522333b
thalles <thallyslira@gmail.com>
2026-02-13 17:30:47 -0300

    Aprendizados em Pyhon

--- 2026/02/Python/5.9-divisor.py
+++ 2026/02/Python/5.9-divisor.py
@@ -5,9 +5,9 @@ valor = a
 while substrato > 0:
     substrato = substrato - 1 
     valor = valor - a 
+print(valor)
 # Calma, calma, calma.
 # O quê eu fiz sem muito cuidado no da multiplicação, terei que fazer em dobro aqui.
 # O quê é a divisão? subtrações consecutivas. 
 # Eu posso dividir 10 por 2 subtraindo 5 vezes.
-# Mas qual é a lógica de "poder diminuir até tal quantidade"? Chegar em zero? 
-
+# Mas qual é a lógica de "poder diminuir até tal quantidade"? Chegar em zero?
